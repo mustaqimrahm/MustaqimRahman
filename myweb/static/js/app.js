633df26ae08bc9ffb76c250b4dd14691aefb81cc
Mustaqim Rahman <mustaqim.rahm@gmail.com>
2021-08-19 05:26:26 -0400

    edited app.js and index.html

--- myweb/static/js/app.js
+++ myweb/static/js/app.js
@@ -1,16 +1,16 @@
 $(function() {
-
+/*
 	// Get the form.
 	var form = $('#ajax-contact');
 
 	// Get the messages div.
 	var formMessages = $('#form-messages');
-
+*/
 	// Set up an event listener for the contact form.
 	$(form).submit(function(e) {
 		// Stop the browser from submitting the form.
 		e.preventDefault();
-
+		/*
 		// Serialize the form data.
 		var formData = $(form).serialize();
 
@@ -20,13 +20,14 @@ $(function() {
 			url: $(form).attr('action'),
 			data: formData
 		})
+		*/
 		.done(function(response) {
 			// Make sure that the formMessages div has the 'success' class.
 			$(formMessages).removeClass('error');
 			$(formMessages).addClass('success');
 
 			// Set the message text.
-			$(formMessages).text(response);
+			//$(formMessages).text(response);
 
 			// Clear the form.
 			$('#name').val('');
@@ -41,7 +42,7 @@ $(function() {
 
 			// Set the message text.
 			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
+				//$(formMessages).text(data.responseText);
 			} else {
 				$(formMessages).text('Oops! An error occured and your message could not be sent.');
 			}
